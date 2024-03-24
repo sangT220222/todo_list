7ae3c607ac6a331919bf3c81e2d9ad6c8ba6a5d3
sangT220222 <magnas9@hotmail.co.uk>
2024-03-24 10:26:10 +0000

    file and folder structure initial setup

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.info("inital setup");
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLDZCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kb19saXN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUuaW5mbyhcImluaXRhbCBzZXR1cFwiKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
